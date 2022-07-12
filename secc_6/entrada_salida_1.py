2d1bc813cff97cec38bbb0491c294e0e7ab7d276
karinaic <karinainche@gmail.com>
2022-07-12 12:06:38 +0200

    cambio_directorio

--- secc_6/entrada_salida_1.py
+++ secc_6/entrada_salida_1.py
@@ -45,3 +45,46 @@ archivo.write('Bienvenido')
 
 archivo.close()
 
+############################################################################
+
+#DIRECTORIOS
+
+'''Para abrir archivos en una ruta de acceso distinta, importaremos el objeto:Path  y Os'''
+
+import os
+
+ruta = os.getcwd()  #este metodo nos da la ruta absoluta al archivo en el que estoy trabajando.
+print(ruta)
+
+
+ruta_1 = os.chdir('C:\\Users\\Karina\\Desktop\\alternativo')  #para abrir archivos que se encuentran en otro lugar, ponemos una ruta distinta
+
+archivo = open('otro_archivo.txt') #abrimos el contenido de este archivo desde una carpeta distinta
+print(archivo.read())
+
+
+print(ruta_1)
+
+#para crear una carpeta nueva
+
+ruta_2 = os.makedirs('C:\\Users\\Karina\\Desktop\\alternativo\\otra')
+
+###############################################################################
+
+import os
+#usando el metodo basename() puedo obteber el nombre base del archivo que intento abrir
+# una ruta completa se compone de ruta al archivo + nombre del archivo
+
+ruta = 'C:\\Users\\Karina\\Desktop\\alternativo\\prueba.txt'
+
+elemento = os.path.basename(ruta)
+
+print(elemento)
+
+#si lo que queremos obtener el primer elemento hasta nuestro archivo, el nombre del directorio.
+elemento_1 = os.path.dirname(ruta)
+
+print(elemento_1)
+
+#para ELIMINAR carpetas:
+os.rmdir('RUTA HASTA LO QUE QUEREMOS ELIMINAR')
